c23bb860fc9c2feb735b0533862973da5c94216e
Victor Caveda <caveda@users.noreply.github.com>
2022-04-19 20:53:41 +0200

    wire up basic MVVM structure

--- app/src/main/java/com/victorcaveda/playground/MainActivity.kt
+++ app/src/main/java/com/victorcaveda/playground/MainActivity.kt
@@ -3,13 +3,19 @@ package com.victorcaveda.playground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.victorcaveda.playground.ui.theme.PlaygroundTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,22 +31,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting()
+                    Stations()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(mainViewModel: MainViewModel = viewModel()) {
-    Text(text = "Hello ${mainViewModel.message}")
+fun Stations(mainViewModel: MainViewModel = viewModel()) {
+    Column(
+    ) {
+        with(mainViewModel.uiState.collectAsState().value) {
+            Listing(this.map { it.name })
+        }
+    }
+}
+
+
+@Composable
+private fun Listing(elements: List<String>) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(elements) { name ->
+            Text(name)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PlaygroundTheme {
-        Greeting()
+        Stations()
     }
 }
--- app/src/main/java/com/victorcaveda/playground/MainViewModel.kt
+++ app/src/main/java/com/victorcaveda/playground/MainViewModel.kt
@@ -1,13 +1,27 @@
 package com.victorcaveda.playground
 
 import androidx.lifecycle.ViewModel
+import com.victorcaveda.domain.model.Station
 import com.victorcaveda.domain.useCase.GetAirQualityDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getAirQualityDataUseCase: GetAirQualityDataUseCase
 ) : ViewModel() {
-    val message = "Hilt ViewModel"
+
+    private val _uiState = MutableStateFlow<List<Station>>(emptyList())
+    val uiState: StateFlow<List<Station>> = _uiState
+
+
+    init {
+        loadData()
+    }
+
+    private fun loadData() {
+        _uiState.value = getAirQualityDataUseCase()
+    }
 }
