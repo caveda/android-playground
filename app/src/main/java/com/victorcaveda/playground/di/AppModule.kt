b9b7044ed123b106432804c84f1dd232aa46534c
Victor Caveda <caveda@users.noreply.github.com>
2022-04-24 20:39:31 +0200

    migrate data module to android library and adding hilt module to hide dependencies from app

--- app/src/main/java/com/victorcaveda/playground/di/AppModule.kt
+++ app/src/main/java/com/victorcaveda/playground/di/AppModule.kt
@@ -1,48 +1,11 @@
 package com.victorcaveda.playground.di
 
-import com.victorcaveda.data.dataSource.openData.WeatherDataService
-import com.victorcaveda.data.repository.AirQualityRepositoryImpl
-import com.victorcaveda.domain.repository.AirQualityRepository
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
-
-    @Provides
-    fun provideRetrofit(): WeatherDataService =
-        Retrofit.Builder()
-            .client(getOkHttpClient())
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(WeatherDataService::class.java)
-
-    private fun getOkHttpClient() =
-        OkHttpClient.Builder()
-            .connectTimeout(NETWORK_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .readTimeout(NETWORK_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .writeTimeout(NETWORK_REQUEST_TIMEOUT_SECONDS, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
-            .build()
-
-    @Provides
-    fun provideAirQualityRepository(): AirQualityRepository = AirQualityRepositoryImpl()
-
-    companion object {
-        const val NETWORK_REQUEST_TIMEOUT_SECONDS = 15L
-        const val BASE_URL = "https://api.openweathermap.org/"
-    }
-
 }
